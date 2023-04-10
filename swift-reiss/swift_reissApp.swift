2163a56c5540bd7da140c463e05ee390a82c4fb0
Eufeem <eufeemgr@gmail.com>
2023-04-09 23:13:35 -0600

    Initial Commit

---
+++ swift-reiss/swift_reissApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_reissApp.swift
+//  swift-reiss
+//
+//  Created by Carlos García on 09/04/23.
+//
+
+import SwiftUI
+
+@main
+struct swift_reissApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
